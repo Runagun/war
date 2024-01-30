18688538d4a46d1239ca6edbcdf260396992288c
Runagun <80551251+Runagun@users.noreply.github.com>
2024-01-30 10:35:50 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,8 +1,7 @@
-let Xpos = 100;
-let Ypos = 100;
-let Xpos2 = 700;
-let Ypos2 = 700;
-let farge = 220;
+let Xpos = 700;
+let Ypos = 700;
+let Xpos2 = 100;
+let Ypos2 = 100;
 let avstand;
 let avstand2;
 let poeng = 0;
@@ -12,64 +11,65 @@ let ath;
 let bg;
 let spar;
 let tr;
-let teller = 0;
-
-
 
 
 function setup() {
-  athImg = loadImage("ath.png");
-  bgImg = loadImage("bg.png");
-  sparImg = loadImage("spar.png");
-  trImg = loadImage("tr.png");
-
   createCanvas(800, 800);
-  
+  ath = loadImage('ath.png');
+  bg = loadImage('bg.png');
+  spar = loadImage('spar.png');
+  tr = loadImage('tr.png');
 }
 
 function draw() {
-  image(bgImg,0,0, 800, 800);
+  image(bg,0,0);
   player1();
   player2();
   kollisjon();
   kollisjon2();
   treasure();
-  winScreen();
-  
-  //counter
-  teller = teller + 1;
-  
+  tekst();
 }
 
 
 
 
-function winScreen() {
+
+
+
+
+
+
+function tekst() {
   
-  text("Poeng: " + poeng, 400, 100);
-  textAlign(CENTER);
-  textSize(32)
-  stroke(0);
-  strokeWeight(6);
   fill("red");
+  stroke(0);
+  strokeWeight(5);
+  textSize(25);
+  textAlign(CENTER);
+  text("Poeng: " + poeng, 400, 100);
   
   if (poeng >= 10) {
-    text("Athenians Win!", 400, 400);
+    textSize(50);
+    text("Spartans WIN!", 400, 400);
+    noLoop();
   }
+  
   if (poeng <= -10) {
-    text("Romans Win!", 400, 400);
+    textSize(50);
+    fill("blue");
+    text("Athenians WIN!", 400, 400);
+    noLoop();
   }
+  
 }
-
-
-
 
 
 function kollisjon2() {
   
   avstand2 = dist(Xpos2, Ypos2, Tx, Ty);
   
-  if (avstand2 <= 20) {
+  if (avstand2 <= 30) {
     
     farge = "green";
     poeng = poeng + 1;
@@ -85,7 +85,7 @@ function kollisjon() {
   
   avstand = dist(Xpos, Ypos, Xpos2, Ypos2);
   
-  if (avstand <= 20) {
+  if (avstand <= 30) {
     
     farge = "black";
     poeng = poeng - 1;
@@ -100,18 +100,16 @@ function kollisjon() {
 
 function treasure() {
   
-  image(trImg, Tx-30, Ty-30);
-  
-    if (teller == 10000 || teller < 0) {
-    Tx = random(30, 770);
-    Ty = random(30, 770);
-  }
+  circle(Tx, Ty, 30);
+  image(tr, Tx-30, Ty-30)
   
 }
 
 function player1() {
   
-  image(sparImg, Xpos-30, Ypos-30);
+  circle(Xpos, Ypos, 30);
+  image(ath,Xpos-30, Ypos-30)
+  
   
   if (keyCode === RIGHT_ARROW) {Xpos = Xpos + 5;}
   if (keyCode === LEFT_ARROW) {Xpos = Xpos - 5;}
@@ -128,7 +126,8 @@ function player1() {
 
 function player2() {
   
-  image(athImg, Xpos2-30, Ypos2-30);
+  circle(Xpos2, Ypos2, 30);
+  image(spar, Xpos2-30, Ypos2-30);
   
   if (keyCode === 68) {Xpos2 = Xpos2 + 5;}
   if (keyCode === 65) {Xpos2 = Xpos2 - 5;}
@@ -147,5 +146,12 @@ function mousePressed() {
   if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
     let fs = fullscreen();
     fullscreen(!fs);
+  }
+}
+
+function keyPressed() {
+  if (keyCode === ENTER) {
+  poeng = 0;
+  loop();
   }
 }
